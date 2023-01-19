089cb752c8c6331092f49f06c6b3456dfb3b23d9
Amol Deshmukh <34318357+des137@users.noreply.github.com>
2023-01-19 13:52:42 -0500

    Update anim.py

--- fokker_planck/anim.py
+++ fokker_planck/anim.py
@@ -13,7 +13,7 @@ def PDF_GBM(x, t, σ=0.2, x0=4, μ=0.1, speed=0.01):
     y = 1/(x * σ * np.sqrt(2 * np.pi * t)) * np.exp(exp_factor)
     return y    
 
-def function2(x, t, σ=0.2, x0=4, μ=0.1, speed=0.01):
+def monte_carlo(x, t, σ=0.2, x0=4, μ=0.1, speed=0.01):
     t = speed * t
     μlog = (μ - σ**2/2)
     x = 4 * np.ones(1000)
@@ -23,7 +23,7 @@ def function2(x, t, σ=0.2, x0=4, μ=0.1, speed=0.01):
 for t in range(100):
     speed = 1/2
     plt.plot(x, PDF_GBM(x, t, speed), color='orange')
-    plt.hist(function2(x, t, speed), density=True, color='blue', 
+    plt.hist(monte_carlo(x, t, speed), density=True, color='blue', 
         alpha=0.7, bins=30)
     plt.ylim(0, 1)
     plt.xlim(0, 20)
